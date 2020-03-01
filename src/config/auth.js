c81aab67916933c50e1c9582f473437f98e83bdb
Octávio Barbosa <40213753+octaviobarbosa@users.noreply.github.com>
2020-03-01 15:02:02 -0300

    modulo node done

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,5 @@
 export default {
-  secret: '497ba19b19c66b03a9ff44492497d556',
+  //secret: '497ba19b19c66b03a9ff44492497d556',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d'
 };
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,12 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,13 +19,23 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware);
+routes.use(authMiddleware); 
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/providers', ProviderController.index); 
+routes.get('/providers/:providerId/available', AvailableController.index); 
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 // module.exports = routes;
 export default routes;
